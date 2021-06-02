f39955084e223af79ed3868988b2d99f173c3bf9
CobbyDev <gui.rodrigues962@gmail.comm>
2021-06-02 11:47:41 -0300

    :collision: nova funcionalidade projeto quase pronto

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ O controle das tarefas desse projeto será realizado no github
 
 ## Ícones
 
-:mag_right: nova funcionalidade Hamburguer
+:mag_right: nova funcionalidade projetos and conhecimento
 :up: atualização
 :honeybee:correção de bug
 :checkered_flag: release
--- public_html/script.js
+++ public_html/script.js
@@ -3,3 +3,29 @@ var hamburguer = document.querySelector(".hamburguer");
 hamburguer.addEventListener("click", function(){
     document.querySelector(".container").classList.toggle("show-menu");
 })
+document.querySelector("#qtde").addEventListener("change", atualizarpreco)
+document.querySelector("#js").addEventListener("change", atualizarpreco)
+document.querySelector("#layout-sim").addEventListener("change", atualizarpreco)
+document.querySelector("#layout-nao").addEventListener("change", atualizarpreco)
+document.querySelector("#prazo").addEventListener("change", function() {
+    const prazo = document.querySelector ("#prazo").value
+    document.querySelector ("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`
+    atualizarpreco ()
+})
+
+function atualizarpreco () {
+    const qtde = document.querySelector("#qtde").value
+    const temjs = document.querySelector("#js").checked
+    const incluiLayout = document.querySelector ("#layout-sim").checked
+    const prazo = document.querySelector ("#prazo").value
+    
+
+    let preco = qtde *50;
+    if (temjs) preco *= 1.1
+    if (incluiLayout) preco += 500
+    let taxaUrgencia = 1 - prazo*0.1;
+    preco *= 1 + taxaUrgencia
+
+    document.querySelector ("#preco").innerHTML = ` R$ ${preco.toFixed(2)}`
+
+}
